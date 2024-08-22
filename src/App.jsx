fb4ac4c2b2fadcda4418df6328f6c9059fed52ad
EL Marsho <emarsho@gmail.com>
2024-08-21 20:40:39 -0500

    add headingText to update taskList.length with correct headingText

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,9 @@ const App = (props) => {
     />
   )
 
+  const taskNoun = taskList.length === 1 ? "task" : "tasks";
+  const headingText = `${taskList.length} ${taskNoun} remaining`
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -32,7 +35,7 @@ const App = (props) => {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
