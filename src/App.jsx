cd35e1ef26d2dc6d57832f1e463f9434949fd6cc
EL Marsho <emarsho@gmail.com>
2024-08-24 10:11:07 -0500

    add Form.jsx functionality add headingText to update taskList.length

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
-// import { nanoid } from "nanoid"
+import { nanoid } from "nanoid"
 
 const App = (props) => {
+  const [tasks, setTasks] = useState(props.tasks)
+  
+  const addTask = (name) => {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false }
+    setTasks([...tasks, newTask])
+  }
 
-  const taskList = props.tasks.map(task =>
+  const taskList = tasks.map(task =>
     <Todo
       key={task.id}
       name={task.name}
@@ -15,16 +22,19 @@ const App = (props) => {
     />
   )
 
+  const taskNoun = taskList.length === 1 ? "task" : "tasks"
+  const headingText = `${taskList.length} ${taskNoun} remaining`
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
