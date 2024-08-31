9bdef48e2c0a4f0ea49fc38beff7a8dab315ac07
EL Marsho <emarsho@gmail.com>
2024-08-31 10:35:23 -0500

    add deleteTask funtionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 
 
-const Todo = ({ name, id, completed, toggleTaskCompleted }) => {
+const Todo = ({ name, id, completed, toggleTaskCompleted, deleteTask }) => {
     return (
         <li className="todo stack-small">
             <div className="c-cb">
@@ -19,7 +19,7 @@ const Todo = ({ name, id, completed, toggleTaskCompleted }) => {
                 <button type="button" className="btn">
                     Edit <span className="visually-hidden">{name}</span>
                 </button>
-                <button type="button" className="btn btn__danger">
+                <button type="button" className="btn btn__danger" onClick={deleteTask}>
                     Delete <span className="visually-hidden">{name}</span>
                 </button>
             </div>
