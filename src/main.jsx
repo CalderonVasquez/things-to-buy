4d5b10d37b328c20920ae6e3e0037e7e7d969662
EL Marsho <emarsho@gmail.com>
2024-09-04 21:48:37 -0500

    add components add Todo.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-/* eslint-disable react-refresh/only-export-components */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 const DATA = [
-  { name: 'Eat', id: 'todo-0', completed: true },
-  { name: 'Sleep', id: 'todo-1', completed: false },
-  { name: 'Pray', id: 'todo-2', completed: false },
+  { name: "Eat", id: "todo-0", completed: true },
+  { name: "Sleep", id: "todo-1", completed: false },
+  { name: "Pray", id: "todo-2", completed: false },
 ]
 
 createRoot(document.getElementById('root')).render(
