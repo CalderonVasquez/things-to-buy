79447c92fe92e8215b0ee23cf199b30f1edb2702
EL Marsho <emarsho@gmail.com>
2024-10-12 21:22:29 -0500

    add Form.jsx functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable react/prop-types */
-// import { useState } from "react";
+import { useState } from "react";
 import Todo from "./components/Todo";
-// import { nanoid } from "nanoid"
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import { nanoid } from "nanoid"
 
 const App = (props) => {
+  const [tasks, setTasks] = useState(props.tasks)
 
-  const taskList = props.tasks.map(task =>
+  const addTask = (name) => {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false }
+    setTasks([...tasks, newTask])
+  }
+
+  const taskList = tasks.map(task =>
     <Todo 
       key={task.id}
       name={task.name}
@@ -14,44 +22,19 @@ const App = (props) => {
     />
   )
 
+  const taskNoun = taskList.length === 1 ? "task" : "tasks"
+  const headingText = `${taskList.length} ${taskNoun} remaining`
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
