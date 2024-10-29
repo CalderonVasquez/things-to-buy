35ba9064a5fba4bcadd712c15a0072bad51b140f
EL Marsho <emarsho@gmail.com>
2024-10-28 21:09:39 -0500

    add deleteTask functionality

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,10 @@ const App = (props) => {
       id === task.id ? {...task, completed: !task.completed} : task))
   }
 
+  const deleteTask = (id) => {
+    setTasks(remainingTasks => remainingTasks.filter(task => id !== task.id))
+  }
+
   const taskList = tasks.map(task =>
     <Todo 
       key={task.id}
@@ -26,6 +30,7 @@ const App = (props) => {
       id={task.id}
       completed={task.completed}
       toggleTaskCompleted={() => toggleTaskCompleted(task.id)}
+      deleteTask={() => deleteTask(task.id)}
     />
   )
 
